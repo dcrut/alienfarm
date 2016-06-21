b11e416bfa263348028014da7d807c3534d0473b
Daniel Crussi and Joe Irons <pair+dcrut+jiron@allstate.com>
2016-06-21 11:52:06 -0500

    added static js files

---
+++ src/controllers/home.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const router = module.exports = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('home/index')
+});
+
+router.get('/about', (req, res) => {
+  res.render('home/about')
+});
+
+router.get('/faq', (req, res) => {
+  res.render('home/faq')
+});
